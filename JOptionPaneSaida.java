296214680767d01b2f335fce8d73f698884e7b2e
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-04-25 22:49:00 -0300

    Estudos em Java

---
+++ JOptionPaneSaida.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class JOptionPaneSaida {
+        public static void main(String[] args) {
+            JOptionPane.showMessageDialog(null,"Saída de dados");
+
+        }
+    }
+
+
---
+++ TesteJOptionPane.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TesteJOptionPane {
+        public static void main(String[] args) {
+            String entrada;
+            entrada = JOptionPane.showInputDialog("Digite um nome: ");
+        }
+    }
+
+
