92853331830dc2250bb828601498a904fdbc0060
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-04-27 21:24:29 -0300

    Estudos em Java e Kotlin.

---
+++ Soma.kt
@@ -0,0 +1,9 @@
+fun main (args :Array<String>){
+    print("Digite o primeiro valor: ")
+    var numero1:Float = readLine()!!.toFloat()
+    print("Digite o segundo valor: ")
+    var numero2:Float = readLine()!!.toFloat()
+
+    println("$numero1 + $numero2 =  ${numero1+numero2}")
+
+}
