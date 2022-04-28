0a15e82c6eebbf976c9b21008e8365685f0670c3
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-04-27 22:48:11 -0300

    Estudos em Java e Kotlin.

---
+++ CalculoParidade.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CalculoParidade {
+    public static void main(String[] args) {
+
+        int numero;
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        numero = leitor.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+            } else {
+                System.out.println("O número é impar.");
+            }
+        }
+    }
+
