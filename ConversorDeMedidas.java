311a6891056485ee554106d00d41f1ac48e0fa16
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-05-01 15:30:25 -0300

    Estudos em Java e Kotlin.

--- ConversorDeMedidas.java
+++ ConversorDeMedidas.java
@@ -4,13 +4,18 @@ public class ConversorDeMedidas {
     public static void main(String[] args) {
         float medida;
         float km;
+        float Hm;
+        float Dam;
+        float cm;
+        float mm;
 
         Scanner leitor = new Scanner(System.in);
         System.out.println("Digite o valor a ser convertido em metros:");
         medida = leitor.nextFloat();
         System.out.println("----------CONVERTENDO----------");
 
-        System.out.println(medida + "Km");
+
+        System.out.println(Math.pow(3) + "Km");
         System.out.println(medida + "Hm");
         System.out.println(medida + "Dam");
 
