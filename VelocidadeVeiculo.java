3d1c0d3752a4850fe8273c4f61087aa88657ea75
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-05-01 23:42:04 -0300

    Estudos em Java e Kotlin.

---
+++ VelocidadeVeiculo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VelocidadeVeiculo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        float vel;
+        System.out.print("Velocidade do carro = ");
+        vel = leitor.nextFloat();
+        if (vel >80){
+            System.out.println("MULTADO");
+        }
+
+            System.out.println("Dirija com cuidado.");
+        }
+    }
+
