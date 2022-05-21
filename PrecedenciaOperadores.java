c2634afea6090d29d33315b4c70995c39aa9e9f0
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-05-20 21:50:21 -0300

    Estudos em Java e Kotlin.

---
+++ PrecedenciaOperadores.java
@@ -0,0 +1,8 @@
+public class PrecedenciaOperadores {
+    public static void main(String[] args) {
+        boolean resultado = !(5 > 3) || (10 >= 10) && (7 < 4);
+        System.out.println("O valor da expressão é: " + resultado);
+
+
+    }
+}
