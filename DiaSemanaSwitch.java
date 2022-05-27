c090e8296a98973067ca3cd8fb19acfbdb24ee7c
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2022-05-26 23:07:59 -0300

    Estudos em Java e Kotlin.

---
+++ DiaSemanaSwitch.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+
+public class DiaSemanaSwitch {
+    public static void main(String[] args) {
+
+        int dia;
+        dia = Integer.parseInt(JOptionPane.showInputDialog("Digite um número de 1 a 7: "));
+
+        switch (dia){
+            case 1:
+                JOptionPane.showMessageDialog(null, "Domingo");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Segunda");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Terça");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Quarta");
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Quinta");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null, "Sexta");
+                break;
+            case 7:
+                JOptionPane.showMessageDialog(null, "Sábado");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Número inválido");
+
+        }
+
+    }
+}
